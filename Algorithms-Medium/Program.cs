8a0b00696e1e4f889b8e7c098fff0727180ac581
TikTaqTo <aday.yezhekhan@fdrive.kz>
2026-09-12 10:31:05 +0500

    initial commit

---
+++ Algorithms-Medium/Program.cs
@@ -0,0 +1,7 @@
+﻿using Algorithms_Medium;
+
+Console.WriteLine("Hello, Leet Code!");
+
+PredictTheWinner test = new PredictTheWinner();
+// Console.WriteLine(test.PredictTheWinnerPlayer([1,5,2]));
+Console.WriteLine(test.PredictTheWinnerPlayer([1,5,233,7]));
